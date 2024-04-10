748714b0377b061fe6b24dafa5ce70749564f5d1
Recep <r.cilrecep@gmail.com>
2024-04-11 00:31:16 +0300

    Implement GooglePeople API & Create people controller

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,64 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PeopleController : Controller
+{
+    private const string ApiUrl = "https://people.googleapis.com/v1/people/me";
+
+    private readonly IConfiguration _configuration;
+    private readonly IRestClient _restClient;
+    
+    public PeopleController(
+        IConfiguration configuration,
+        IRestClient restClient)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        _restClient = restClient ?? throw new ArgumentNullException(nameof(restClient));
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        var accessToken = GetAccessToken();
+        
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            return Unauthorized("Unauthorized: Access token is invalid or missing.");
+        }
+        
+        var request = new RestRequest(ApiUrl);
+        request.AddQueryParameter ("key", _configuration["GoogleAPI:Key"]);
+        request.AddQueryParameter ("personFields", "emailAddresses,birthdays");
+        request.AddHeader("Authorization", $"Bearer {accessToken}");
+        request.AddHeader ("Accept", "application/json");
+        
+        var response = await _restClient.ExecuteGetAsync(request);
+
+        if (response.StatusCode == HttpStatusCode.OK 
+            && !string.IsNullOrEmpty(response.Content))
+        {
+            var jsonDocument = JsonDocument.Parse(response.Content); 
+            return Ok(jsonDocument);
+        }
+        
+        return StatusCode((int)response.StatusCode);
+    }
+    
+    private string? GetAccessToken()
+    {
+        var authorizationHeader = HttpContext.Request.Headers["Authorization"].ToString();
+
+        if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
+        {
+            return null;
+        }
+
+        return authorizationHeader["Bearer ".Length..];
+    }
+}
